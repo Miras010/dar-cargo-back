6e56478f28deec9fc61de61367d10117c775a404
m.bayzhigit <m.bayzhigit@alterapp.net>
2024-06-10 17:07:55 +0500

    first commit

---
+++ models/File.js
@@ -0,0 +1,14 @@
+const {Schema, model} = require('mongoose')
+
+const File = new Schema({
+    name: {type: String, required: true},
+    date: {type: Date},
+    statusKey: {type: String},
+    statusValue: {type: String},
+    shippedCity: {type: String},
+    type: {type: String, required: true},
+    size: {type: String},
+    createdDate: {type: Date, default: Date.now, required: true},
+})
+
+module.exports = model('File', File)
---
+++ models/Track.js
@@ -0,0 +1,16 @@
+const {Schema, model, ObjectId} = require('mongoose')
+
+const Track = new Schema({
+    trackNumber: {type: String, required: true, unique: true},
+    fileName: String,
+    receivedInChinaDate: Date,
+    receivedInAlmatyDate: Date,
+    fromChinaToAlmaty: Date,
+    receivedByClient: Date,
+    shippedFromAlmatyDate: Date,
+    shippedCity: String,
+    createdDate: {type: Date, default: Date.now, required: true},
+    createdBy: {type: ObjectId, required: true, ref: 'User'},
+})
+
+module.exports = model('Track', Track)
